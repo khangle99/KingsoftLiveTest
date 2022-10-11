5b901a938fcc686ff74ed4243c3c43390d710f3e
khangle99 <dev.khangle@gmail.com>
2022-10-11 17:55:28 +0700

    Initial Commit

---
+++ KingsoftLiveTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  KingsoftLiveTest
+//
+//  Created by Khang L on 11/10/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
